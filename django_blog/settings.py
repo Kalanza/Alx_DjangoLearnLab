9efd4caacf25b2deeed980b756424ca71f9230fe
Kalanza <kalanzavictor@gmail.com>
2025-08-28 23:16:09 +0300

    feat: Add initial Django blog project structure with models, views, templates, and static files

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "django_blog.urls"
+ROOT_URLCONF = "urls"
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "django_blog.wsgi.application"
+WSGI_APPLICATION = "wsgi.application"
 
 
 # Database
