6e2da632e383c5623793bd7b61b6d831d0c09268
Kalanza <kalanzavictor@gmail.com>
2025-09-26 12:06:19 +0300

    feat: Add authentication views and templates for login, logout, and registration

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+class SignUpView(CreateView):
+    form = UserCreationForm
+    success_urk= reverse_lazy('login')
+    template_name = ('signup/registration.html')
