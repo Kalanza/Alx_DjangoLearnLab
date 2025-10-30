f4a5b169c0ca3a0f70c2d70a289c22a4c03a30df
Kalanza <kalanzavictor@gmail.com>
2025-10-30 21:40:09 +0300

    feat: Implement BookList view for listing and creating books

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
-
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
 # Create your views here.
+class BookList(generics.ListCreateAPIView):
+    serializer_class = BookSerializer
+    queryset = Book.objects.all()
