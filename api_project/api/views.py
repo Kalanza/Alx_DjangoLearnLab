e24995c252c432ed44534646a7766aa77051cbed
Kalanza <kalanzavictor@gmail.com>
2025-10-30 23:15:55 +0300

    feat: Add MyAPIView with authentication and permission classes for secure access

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,7 +1,11 @@
 from rest_framework import generics,viewsets
-
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
 # Create your views here.
 class BookList(generics.ListAPIView):
     serializer_class = BookSerializer
@@ -10,3 +14,11 @@ class BookList(generics.ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+class MyAPIView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self,request):
+        #Only authenticated users can access this view
+        return Response({'message':'Hello, authenticated user!'})
